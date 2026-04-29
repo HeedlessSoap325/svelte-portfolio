05b705458940824ee75f309be7d2851bbba8875f
HeedlessSoap325 <mathisjulian123@gmail.com>
2026-04-29 21:05:18 +0200

    feat: implement hamburger menu for mobile

---
+++ src/lib/assets/HamburgerMenuSVG.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	const {width, height, color}: {width: string, height: string, color: string} = $props();
+</script>
+
+<!-- Source: https://www.svgrepo.com/svg/489508/menu-burger-horizontal -->
+<!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<g clip-path="url(#clip0_429_11066)">
+		<path d="M3 6.00092H21M3 12.0009H21M3 18.0009H21" stroke={color} stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+	</g>
+	<defs>
+		<clipPath id="clip0_429_11066">
+			<rect width="24" height="24" fill="white" transform="translate(0 0.000915527)"/>
+		</clipPath>
+	</defs>
+</svg>
--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
 	import { page } from '$app/state';
+    import HamburgerMenuSVG from '$lib/assets/HamburgerMenuSVG.svelte';
     import { paths } from '$lib/path';
+
+	let hamburgerMenuActive: boolean = $state(false);
+
+	function onLinkClicked() {
+		setTimeout(() => {
+			hamburgerMenuActive = false;
+		}, 250)
+	}
 </script>
 
-<nav>
-	<a href={resolve("/")} class="nav-logo">heedlesssoap<span class="dot">.</span>dev</a>
-	<ul class="nav-links">
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<nav class:hamburger={hamburgerMenuActive}>
+	<div class="navs">
+		<div class="hamburger-menu" role="button" tabindex="0" onclick={() => {hamburgerMenuActive = !hamburgerMenuActive}}><HamburgerMenuSVG width="32px" height="32px" color="var(--accent)"></HamburgerMenuSVG></div>
+		<a href={resolve("/")} class="nav-logo">heedlesssoap<span class="dot">.</span>dev</a>
+	</div>
+	<ul class="nav-links" class:hamburger={hamburgerMenuActive}>
 		{#each paths as path (path.name)}
-			<li> <a href={resolve(path.href)} class:active={page.url.pathname === path.href}>{path.name}</a> </li>	
+			<li> <a href={resolve(path.href)} onclick={onLinkClicked} class:active={page.url.pathname === path.href}>{path.name}</a> </li>	
 		{/each}
 	</ul>
 </nav>
@@ -29,6 +42,10 @@
 		backdrop-filter: blur(16px);
 		-webkit-backdrop-filter: blur(16px);
   	}
+
+	.hamburger-menu {
+		display: none;
+	}
 
 	.nav-logo {
 		font-family: var(--display);
@@ -76,5 +93,43 @@
 
 	.nav-links a:hover::after, .nav-links a.active::after { 
 		width: 100%; 
+	}
+
+	@media (max-width: 640px) {
+		nav {
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			padding-inline: 0;
+		}
+
+		nav.hamburger {
+			height: fit-content;
+		}
+
+		.navs {
+			position: fixed;
+			top: 1.2rem;
+			height: 2rem;
+			display: flex;
+			flex-direction: row;
+			justify-content: space-around;
+			width: 100dvw;
+		}
+
+		.hamburger-menu {
+			display: flex;
+		}
+
+		.nav-links {
+			display: none;
+		}
+
+		.nav-links.hamburger {
+			margin-top: 3rem;
+			display: flex;
+			flex-direction: column;
+			gap: 1rem;
+		}
 	}
 </style>
